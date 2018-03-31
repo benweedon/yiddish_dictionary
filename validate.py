e9799c2997a228bce361f9fb8b45f21ef9c64b84
Ben Weedon <ben.weedon@outlook.com>
2018-03-31 00:29:42 -0700

    Move printing of failure message into fail_validation

--- validate.py
+++ validate.py
@@ -2,7 +2,8 @@ import io
 from normalize import normalize
 import sys
 
-def fail_validation():
+def fail_validation(msg):
+    print(msg, file=sys.stderr)
     print()
     print('This file is not valid :(')
     sys.exit(1)
@@ -13,7 +14,6 @@ if __name__ == '__main__':
         normalize(f, output)
         f.seek(0)
         if output.getvalue() != f.read():
-            print('The file isn\'t normalized. Run normalize.py and try again!', file=sys.stderr)
-            fail_validation()
+            fail_validation('The file isn\'t normalized. Run normalize.py and try again!')
 
     print('This file is valid! :)')
