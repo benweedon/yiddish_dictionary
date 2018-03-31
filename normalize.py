5521cfeb87b122c90f46b8247bd5af6a639afd74
Ben Weedon <ben.weedon@outlook.com>
2018-03-31 00:01:41 -0700

    Properly sort normalized dictionary alphabetically
    Before, it was just sorted by unicode value, which for example put the
    various forms of alef far apart.

--- normalize.py
+++ normalize.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 import json
 import re
 import sys
@@ -25,6 +26,60 @@ def replace_combining_chars(s):
     s = s.replace('ת\u05BC', 'תּ')
     return s
 
+def sort_yiddish(yiddish):
+    yiddish_to_key = {
+        'א': 'A',
+        'אַ': 'B',
+        'אָ': 'C',
+        'ב': 'D',
+        'בּ': 'E',
+        'בֿ': 'F',
+        'ג': 'G',
+        'ד': 'H',
+        'ה': 'I',
+        'ו': 'J',
+        'וּ': 'K',
+        'וֹ': 'L',
+        'װ': 'M',
+        'ױ': 'N',
+        'ז': 'O',
+        'ח': 'P',
+        'ט': 'Q',
+        'י': 'R',
+        'יִ': 'S',
+        'ײ': 'T',
+        'ײַ': 'U',
+        'כּ': 'V',
+        'כ': 'W',
+        'ך': 'X',
+        'ל': 'Y',
+        'מ': 'Z',
+        'ם': 'a',
+        'נ': 'b',
+        'ן': 'c',
+        'ס': 'd',
+        'ע': 'e',
+        'פ': 'f',
+        'פּ': 'g',
+        'פֿ': 'h',
+        'ף': 'i',
+        'צ': 'j',
+        'ץ': 'k',
+        'ק': 'l',
+        'ר': 'm',
+        'ש': 'n',
+        'שׂ': 'o',
+        'תּ': 'p',
+        'ת': 'q',
+    }
+    key = ''
+    for char in yiddish:
+        if char in yiddish_to_key:
+            key += yiddish_to_key[char]
+        else:
+            key += char
+    return key
+
 if __name__ == '__main__':
     # load the json into an object
     with open(sys.argv[1], encoding='utf-8') as f:
@@ -43,6 +98,9 @@ if __name__ == '__main__':
     # remove the empty key
     d.pop('', None)
 
+    # sort the dictionary
+    d = OrderedDict(sorted(d.items(), key=lambda i: sort_yiddish(i[0])))
+
     # output the json back to a file
     with open(sys.argv[2], 'w', encoding='utf-8') as f:
-        json.dump(d, f, ensure_ascii=False, indent=4, sort_keys=True)
+        json.dump(d, f, ensure_ascii=False, indent=4)
