3ebe2d22fff7904637c6d1be72bd579bd254c6f4
Ben Weedon <ben.weedon@outlook.com>
2018-03-31 13:37:18 -0700

    Start using standard pos in normalization

--- normalize.py
+++ normalize.py
@@ -26,6 +26,28 @@ def replace_combining_chars(s):
     s = s.replace('ת\u05BC', 'תּ')
     return s
 
+def replace_pos(entry):
+    new_entry = {}
+    new_entry['eng'] = entry['eng']
+    new_entry['_pro'] = entry['_pro']
+    new_entry['_pos'] = []
+    for pos in entry['_pos']:
+        new_pos = pos
+        if pos == '(adj.)':
+            new_pos = 'adj'
+        elif pos == '(adv.)':
+            new_pos = 'adv'
+        elif pos == '(f.)':
+            new_pos = 'f'
+        elif pos == '(m.)':
+            new_pos = 'm'
+        elif pos == '(n.)':
+            new_pos = 'n'
+        elif pos == '(name)':
+            new_pos = 'name'
+        new_entry['_pos'].append(new_pos)
+    return new_entry
+
 def normalize(input, output):
     # load the json into an object
     d = json.load(input)
@@ -33,12 +55,15 @@ def normalize(input, output):
     # normalize yiddish strings
     for yiddish in d.copy():
         new_yiddish = replace_combining_chars(yiddish)
+        new_entry = replace_pos(d[yiddish])
         if new_yiddish not in d:
-            d[new_yiddish] = d[yiddish]
+            d[new_yiddish] = new_entry
             d.pop(yiddish, None)
         elif new_yiddish != yiddish:
-            d[new_yiddish] = utils.combine_entries(d[yiddish], d[new_yiddish])
+            d[new_yiddish] = utils.combine_entries(new_entry, d[new_yiddish])
             d.pop(yiddish, None)
+        else:
+            d[new_yiddish] = new_entry
 
     # remove the empty key
     d.pop('', None)
